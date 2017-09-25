88741dbd4670cdd1fd25fb45f3e8b7afc24d27c5
Adam Fudge <coolfudge@gmail.com>
2017-09-25 18:59:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,16 +1,23 @@
 def badge_maker(name)
-  puts "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(name_array)
+  badge_array = []
   name_array.each do |name|
-    badge_maker(name)
+    badge_array << badge_maker(name)
   end
+  return badge_array
 end
 
 def assign_rooms(name_array)
+  room_array = []
   name_array.each_with_index do |name, index|
     room_number = index + 1
     puts "Hello, #{name}! You'll be assigned to room #{room_number}"
   end
+end
+
+def printer(name_array)
+  
 end
